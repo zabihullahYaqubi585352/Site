da1d1a8609d3d8e5f6d70e912e1292c8cbd54e92
Nasim <40519831+Naseq@users.noreply.github.com>
2025-05-21 09:51:16 +0430

    transfer loacle

--- resources/js/components/i18n.jsx
+++ resources/js/components/i18n.jsx
@@ -1,7 +1,7 @@
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
-import en from '../../views/locales/en/translation.json';
-import fa from '../../views/locales/fa/translation.json';
+import en from '../../locales/en/translation.json';
+import fa from '../../locales/fa/translation.json';
 
 i18n.use(initReactI18next).init({
     resources: {
