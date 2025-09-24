87b73aa0058f34ff5f205fda4fccc0045a7dcdf8
Nasim <40519831+Naseq@users.noreply.github.com>
2025-09-24 09:21:40 +0430

    Update i18n

--- resources/js/components/i18n.jsx
+++ resources/js/components/i18n.jsx
@@ -1,7 +1,7 @@
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
-import en from '../../locales/en/translation.json';
-import fa from '../../locales/fa/translation.json';
+import en from '../../locales/en/Translation.json';
+import fa from '../../locales/fa/Translation.json';
 
 i18n.use(initReactI18next).init({
     resources: {
